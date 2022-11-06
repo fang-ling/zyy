7aec884eca679382627610cf0a0548dfa856a752
fangling <alice@jxx.world>
2022-11-06 15:14:52 +0800

    (lib) add cmark-0.29.0.gfm.6

---
+++ README.md
@@ -0,0 +1,3 @@
+# zyy
+
+A description of this package.
---
+++ Sources/zyy/zyy.swift
@@ -0,0 +1,8 @@
+@main
+public struct zyy {
+    public private(set) var text = "Hello, World!"
+
+    public static func main() {
+        print(zyy().text)
+    }
+}
