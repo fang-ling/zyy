41573758e757fe4a2cae7a712ff4561cb3b52a02
fangling <alice@jxx.world>
2022-11-20 19:34:08 +0800

    add command `init`
    Still have some tables not created.

--- Sources/zyy/zyy.swift
+++ Sources/zyy/zyy.swift
@@ -1,7 +1,10 @@
+import Foundation
+
 @main
 public struct zyy {
     public private(set) var text = "Hello, World!"
     
+    /* Command Line related String constants */
     private static let VERSION = "0.0.1-pre_alpha"
     private static let WELCOME_MSG =
 """
@@ -11,11 +14,21 @@ Usage: zyy <command> [<switches>...]
   init:                 Initialize website database in current directory
   version:              Print version information and exit
 """
+    /* Databse filename(not user changeable) */
+    private static let DB_FILENAME = "zyy.db"
     
     public static func main() {
         if CommandLine.arguments.count > 1 {
             if CommandLine.arguments[1] == "init" {
-                
+                /* Prevent user from calling `init` twice */
+                if FileManager.default.fileExists(atPath: DB_FILENAME) {
+                    generalError(msg: "Database file: \(DB_FILENAME) existed")
+                }
+                /* Create db */
+                createDatabase()
+                print("Creating \(DB_FILENAME)")
+                print("You may want to invoke `zyy configure` command to " +
+                      "finish setting up your website")
             } else if CommandLine.arguments[1] == "version" {
                 print(VERSION)
             } else {
@@ -25,5 +38,18 @@ Usage: zyy <command> [<switches>...]
         } else {
             print(WELCOME_MSG)
         }
+    }
+    
+    private static func createDatabase() {
+        let SETTINGS_SQL =
+                           """
+                           CREATE TABLE if not exists Settings(
+                                 field    TEXT    PRIMARY KEY    NOT NULL,
+                                 value    TEXT
+                           );
+                           """
+        let sqlite = SQLite(at: DB_FILENAME)
+        sqlite.exec(sql: SETTINGS_SQL)
+        sqlite.SQLite3_close()
     }
 }
