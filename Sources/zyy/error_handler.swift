b695ef989e11e6c834ab02144eacbccee7e1d652
fangling <alice@jxx.world>
2022-11-20 19:15:50 +0800

    add a general error handling wrapper

--- Sources/zyy/error_handler.swift
+++ Sources/zyy/error_handler.swift
@@ -5,10 +5,20 @@
 //  Created by Fang Ling on 2022/11/20.
 //
 
+/* Error-handling wrappers allow us to keep code concise, and after calling one
+ * of the following functions, `zyy` will print some messages and exit. */
+
 import Foundation
 
+/* Command Line releated error */
 func commandLineError(msg : String) {
     print("Command Line Error:")
+    print(msg)
+    exit(-1)
+}
+
+func generalError(msg : String) {
+    print("Error:")
     print(msg)
     exit(-1)
 }
