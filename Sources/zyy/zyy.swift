0f18030f5e642145ab61822d9b3be906700d4cb1
fangling <alice@jxx.world>
2022-11-21 12:57:27 +0800

    start implement `configure` subcommand
    add getSetting() and setSetting() to retrieve / write to the Setting table
    add interactive command to modify site name field

--- Sources/zyy/zyy.swift
+++ Sources/zyy/zyy.swift
@@ -12,10 +12,18 @@ Usage: zyy <command> [<switches>...]
 
 <Commands>
   init:                 Initialize website database in current directory
+  configure:            Set up the website
   version:              Print version information and exit
 """
     /* Databse filename(not user changeable) */
     private static let DB_FILENAME = "zyy.db"
+    /* Database table name */
+    private static let DB_SETTING_TABLE_NAME = "Setting"
+    /* Database table column name */
+    private static let DB_SETTING_TABLE_COL_FIELD = "field"
+    private static let DB_SETTING_TABLE_COL_VALUE = "value"
+    /* Setting table field names */
+    private static let DB_SETTING_FIELD_SITENAME = "sitename"
     
     public static func main() {
         if CommandLine.arguments.count > 1 {
@@ -29,6 +37,23 @@ Usage: zyy <command> [<switches>...]
                 print("Creating \(DB_FILENAME)")
                 print("You may want to invoke `zyy configure` command to " +
                       "finish setting up your website")
+            } else if CommandLine.arguments[1] == "configure" {
+                /* Interactive settings */
+                /* Check if database exists */
+                if !FileManager.default.fileExists(atPath: DB_FILENAME) {
+                    generalError(msg: "Database file: \(DB_FILENAME): " +
+                                      "No such file or directory")
+                }
+                print("Hint: Press enter directly to leave it as-is")
+                /* Website name*/
+                var site_name = getSetting(field: DB_SETTING_FIELD_SITENAME)
+                print("What's the name of your site[\(site_name)]: ")
+                site_name = readLine() ?? site_name /* May be unnecessary */
+                if site_name != "" { /* User input something */
+                    setSetting(field: DB_SETTING_FIELD_SITENAME,
+                               value: site_name)
+                }
+                
             } else if CommandLine.arguments[1] == "version" {
                 print(VERSION)
             } else {
@@ -41,15 +66,60 @@ Usage: zyy <command> [<switches>...]
     }
     
     private static func createDatabase() {
-        let SETTINGS_SQL =
-                           """
-                           CREATE TABLE if not exists Settings(
-                                 field    TEXT    PRIMARY KEY    NOT NULL,
-                                 value    TEXT
-                           );
-                           """
+        let SQL = """
+                  CREATE TABLE if not exists \(DB_SETTING_TABLE_NAME)(
+                      \(DB_SETTING_TABLE_COL_FIELD) TEXT PRIMARY KEY NOT NULL,
+                      \(DB_SETTING_TABLE_COL_VALUE) TEXT
+                  );
+                  """
         let sqlite = SQLite(at: DB_FILENAME)
-        sqlite.exec(sql: SETTINGS_SQL)
+        sqlite.exec(sql: SQL)
         sqlite.SQLite3_close()
+    }
+    
+    /* Add a new setting in table, and will replace the old one if exists. */
+    private static func setSetting(field : String, value : String) {
+        /* See: https://stackoverflow.com
+         *      /questions/3634984/insert-if-not-exists-else-update
+         * Use the `INSERT OR IGNORE` followed by an `UPDATE`.
+         */
+        var SQL = """
+                  INSERT OR IGNORE INTO \(DB_SETTING_TABLE_NAME) (field, value)
+                  VALUES(
+                      '\(field)', '\(value)'
+                  );
+                  """
+                  
+        let sqlite = SQLite(at: DB_FILENAME)
+        sqlite.exec(sql: SQL)
+            SQL = """
+                  UPDATE \(DB_SETTING_TABLE_NAME)
+                  SET \(DB_SETTING_TABLE_COL_VALUE) = '\(value)'
+                  WHERE \(DB_SETTING_TABLE_COL_FIELD) = '\(field)';
+                  """
+        sqlite.exec(sql: SQL)
+        sqlite.SQLite3_close()
+    }
+    
+    /* Get a setting value from given `field`. */
+    private static func getSetting(field : String) -> String {
+        let SQL = """
+                  SELECT * FROM \(DB_SETTING_TABLE_NAME)
+                  WHERE field = '\(field)';
+                  """
+        let sqlite = SQLite(at: DB_FILENAME)
+        let result = sqlite.exec(sql: SQL)
+        var value = ""
+        /* Already find result in SQL, so result.count should be either 0 or 1*/
+        if let row = result.first {
+            /* Will result a String?? (nasty) */
+            if let val = row[DB_SETTING_TABLE_COL_VALUE] {
+                if let v = val {
+                    value = v
+                }
+            }
+        }
+        sqlite.SQLite3_close()
+        return value
     }
 }
