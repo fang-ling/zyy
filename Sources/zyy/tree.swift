dc0e911a0b7d5dc049d656aa8771d735fbf70e38
fangling <alice@jxx.world>
2023-01-05 13:13:02 +0800

    modify tree API

--- Sources/zyy/tree.swift
+++ Sources/zyy/tree.swift
@@ -6,22 +6,26 @@
 //
 
 class TreeNode {
-//    var p : TreeNode?
-    var child : [TreeNode?]
+    var child : [TreeNode]
     /* Actual payload, html tag */
     var begin : String
     var end : String
     
     init() {
-//        p = nil
-        child = [TreeNode?]()
+        child = [TreeNode]()
         begin = ""
         end = ""
     }
     
     init(begin : String, end : String) {
-//        self.p = p
-        self.child = [TreeNode?]()
+        self.child = [TreeNode]()
+        self.begin = begin
+        self.end = end
+    }
+    
+    init(begin : String, content : String, end : String) {
+        self.child = [TreeNode]()
+        self.child.append(TreeNode(begin: content, end: ""))
         self.begin = begin
         self.end = end
     }
@@ -40,8 +44,7 @@ class Tree {
     
     /* Insert a node to a subtree */
     static func subtree_insert(_ subtree : TreeNode,
-                               begin : String,
-                               end : String) {
-        subtree.child.append(TreeNode(begin: begin, end: end))
+                               node : TreeNode) {
+        subtree.child.append(node)
     }
 }
