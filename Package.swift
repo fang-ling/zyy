e1973b167647f79011bb05599a5f74ba1d34e904
Fang Ling <alice@jxx.world>
2023-02-23 21:56:30 +0800

    (lib) add swift-argument-parser

--- Package.swift
+++ Package.swift
@@ -1,5 +1,6 @@
 // swift-tools-version: 5.6
-// The swift-tools-version declares the minimum version of Swift required to build this package.
+// The swift-tools-version declares the minimum version of Swift required to
+// build this package.
 
 import PackageDescription
 
@@ -7,16 +8,17 @@ let package = Package(
     name: "zyy",
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
+        .package(url: "https://github.com/apple/swift-argument-parser.git",
+                 from: "1.2.2")
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .executableTarget(
             name: "zyy",
             dependencies: [
                 "CMark",
-                "CSQLite"
+                "CSQLite",
+                .product(name: "ArgumentParser",
+                         package: "swift-argument-parser")
             ]),
         .target(
             name: "CMark",
